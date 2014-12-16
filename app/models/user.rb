63e552480820e97ff2a54b412c0f88ecd0f304ad
LuckOfWise <luck.of.wise@gmail.com>
2014-12-16 11:20:01 +0900

    喋らせる

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,15 +12,16 @@ class User < ActiveRecord::Base
     self.verification_code = 4.times.map { Random.rand(9) + 1 }.join
     self.verified_at = nil
     self.save!
-    Nexmo::Client.new.send_message(from: 'Ruby', to: phone_number_to_send, text: send_message_text, type: 'unicode' )
+    Nexmo::Client.new.send_message(from: 'Ruby', to: phone_number_to_send, text: send_message_text, type: 'unicode')
   end
 
   def call_verification_code!
-    Nexmo::Client.new.initiate_tts_call(from: 'Ruby', to: phone_number_to_send, text: send_message_text, type: 'unicode' )
+    res = Nexmo::Client.new.initiate_tts_call(from: 'Ruby', to: phone_number_to_send, lg: 'ja-jp', text: "認証コード: #{self.verification_code.split(//).join(',')} この番号を画面に入力してください。nexmo-sample", type: 'unicode')
+    Rails.logger.info res
   end
 
   def phone_number_to_send
-    "+81#{self.phone_number.gsub('-', '')[1..10]}"
+    "81#{self.phone_number.gsub('-', '')[1..10]}"
   end
 
   def send_message_text
